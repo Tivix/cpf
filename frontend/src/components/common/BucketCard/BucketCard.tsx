59bbf8f9515c9f777e8874fbacd826cff7e2aa0a
Wiktoria Salamon <wiktoriajuliasalamon@gmail.com>
2024-04-25 13:57:10 +0200

    feat: ladder details style improvements [gh-10]

--- frontend/src/components/common/BucketCard/BucketCard.tsx
+++ frontend/src/components/common/BucketCard/BucketCard.tsx
@@ -1,5 +1,6 @@
 import Link from 'next/link';
 import { BucketCardProps } from './BucketCard.interface';
+import {ChevronRightIcon} from "@app/static/icons/ChevronRightIcon";
 
 export const BucketCard = ({ bucket }: BucketCardProps) => {
     const { bucketSlug, bucketName, description } = bucket;
@@ -9,7 +10,12 @@ export const BucketCard = ({ bucket }: BucketCardProps) => {
             href={`/library/backend/${bucketSlug}`}
             className="border border-navy-200 bg-white rounded-2xl flex cursor-pointer hover:bg-navy-100 flex-col p-6 gap-2"
         >
-            <h2 className="text-navy-900 text-xl font-semibold">{bucketName}</h2>
+            <div className="flex justify-between">
+                <h2 className="text-navy-900 text-xl font-semibold">{bucketName}</h2>
+                <div className="rounded-full bg-navy-50 w-8 h-8 flex justify-center items-center">
+                    <ChevronRightIcon />
+                </div>
+            </div>
             <p className="text-navy-600 text-base">{description}</p>
         </Link>
     );
--- frontend/src/components/modules/LadderDetails/LadderDetails.tsx
+++ frontend/src/components/modules/LadderDetails/LadderDetails.tsx
@@ -14,19 +14,34 @@ export const LadderDetails = ({ ladder, ladderName, band }: LadderDetailsProps) 
               <p className="text-navy-600 text-l font-medium">Salary range: {ladder.salaryRange}</p>
           </div>
           <div className="rounded-xl border border-[#E5E6EA] p-4 flex flex-col items-center">
-              <div className="flex gap-2 justify-between">
+              <div className="flex gap-2 justify-between text-navy-600">
                   <p>Threshold</p>
-                  <InfoIcon />
+                  <InfoIcon className="text-navy-600" />
               </div>
-              <p className="text-2xl">{ladder.threshold}</p>
+              <p className="text-2xl font-bold">{ladder.threshold}</p>
           </div>
       </div>
-        <div>
-            <p className="text-m text-navy-600">Buckets you need to complete to get on this band:</p>
-            <p className="text-sm text-navy-600 uppercase">Hard skills</p>
-            {ladder.buckets.map((bucket: Bucket) => (
-                <BucketCard bucket={bucket} />
-            ))}
+        <div className="flex flex-col gap-6">
+            <p className="text-m text-navy-600 tracking-wide">Buckets you need to complete to get on this band:</p>
+            <div className="flex flex-col gap-4">
+                <p className="text-sm text-navy-600 uppercase tracking-wide">Hard skills</p>
+                <div className="flex flex-col gap-6">
+                    { /* TODO: use only hard skills */ }
+                    {ladder.buckets.map((bucket: Bucket) => (
+                        <BucketCard bucket={bucket} />
+                    ))}
+                </div>
+            </div>
+            <div className="flex flex-col gap-4">
+                <p className="text-sm text-navy-600 uppercase tracking-wide">Soft skills</p>
+                <div className="flex flex-col gap-6">
+                    { /* TODO: use only soft skills */ }
+                    {ladder.buckets.map((bucket: Bucket) => (
+                        /* TODO: use collapsible card */
+                        <BucketCard bucket={bucket} />
+                    ))}
+                </div>
+            </div>
         </div>
     </div>
   );
