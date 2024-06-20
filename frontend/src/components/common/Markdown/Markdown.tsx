ae5d2b67afdefc6de08017f095a850f6adad381f
Wiktoria Salamon <wiktoriajuliasalamon@gmail.com>
2024-06-20 14:33:54 +0200

    linter

--- frontend/src/components/common/Markdown/Markdown.tsx
+++ frontend/src/components/common/Markdown/Markdown.tsx
@@ -1,7 +1,7 @@
 import Markdown from 'react-markdown';
 import remarkGfm from 'remark-gfm';
 import { FC } from 'react';
-import {MarkdownProps} from "@app/components/common/Markdown/Markdown.interface";
+import { MarkdownProps } from '@app/components/common/Markdown/Markdown.interface';
 
 export const CustomMarkdown: FC<MarkdownProps> = ({ text }) => {
   return <Markdown remarkPlugins={[remarkGfm]}>{text}</Markdown>;
