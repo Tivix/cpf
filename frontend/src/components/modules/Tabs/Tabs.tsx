836244fd39983b567bd71db15214365e2d227661
Jakub Folta <jakubfolta@yahoo.co.uk>
2024-06-20 14:22:59 +0200

    feat: update tabs styling [gh-36]

--- frontend/src/components/modules/Tabs/Tabs.tsx
+++ frontend/src/components/modules/Tabs/Tabs.tsx
@@ -3,11 +3,11 @@ import { TabsProps } from './Tabs.interface';
 
 export const Tabs = ({ tabs, active, setActive, className }: TabsProps) => (
   <nav className={`flex ${className}`} aria-label="Tab">
-    <ol role="list" className="flex space-x-2 gap-4 pt-4">
+    <ol role="list" className="flex gap-8 pt-4">
       {tabs.map((tab, index) => (
         <li key={index}>
           <div
-            className={generateClassNames('flex flex-col pb-4  cursor-pointer', {
+            className={generateClassNames('flex flex-col px-2 pb-4 font-medium text-navy-500 cursor-pointer', {
               'border-b-2 border-blue-800 text-blue-800': tab.title === active,
             })}
             onClick={() => setActive(tab.title)}
