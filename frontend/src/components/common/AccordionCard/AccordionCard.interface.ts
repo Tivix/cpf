330655794583b9d92e64ec5eeb195cbd159956c5
Wiktoria Salamon <wiktoriajuliasalamon@gmail.com>
2024-06-20 14:57:18 +0200

    fix: accordion card type

--- frontend/src/components/common/AccordionCard/AccordionCard.interface.ts
+++ frontend/src/components/common/AccordionCard/AccordionCard.interface.ts
@@ -1,4 +1,5 @@
-export interface AccordionCardProps extends Partial<HTMLDivElement> {
+export interface AccordionCardProps {
   title: string;
   expandedByDefault?: boolean;
+  className?: string;
 }
