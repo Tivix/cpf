e69461d7a0695280a5f9a2c90032a21ae5c147e9
Wiktoria Salamon <wiktoriajuliasalamon@gmail.com>
2024-06-27 15:25:38 +0200

    chore: remove redundant file

--- frontend/src/types/styled.d.ts
+++
@@ -1,8 +0,0 @@
-import 'styled-components';
-import { theme } from '@app/styles/theme';
-
-export type Theme = typeof theme;
-
-declare module 'styled-components' {
-  export interface DefaultTheme extends Theme {}
-}
