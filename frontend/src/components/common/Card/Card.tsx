dc4ca9d6c39e9861d4b71b95a5e957e8236c43a0
Wiktoria Salamon <wiktoriajuliasalamon@gmail.com>
2024-06-27 15:36:32 +0200

    chore: add missing class

--- frontend/src/components/common/Card/Card.tsx
+++ frontend/src/components/common/Card/Card.tsx
@@ -3,7 +3,7 @@ import { PropsWithChildren } from 'react';
 
 export const Card: React.FC<PropsWithChildren<CardProps>> = ({ title, children }) => {
   return (
-    <div className="rounded-xl bg-white shadow-sm ring-1 ring-navy-200/5">
+    <div className="rounded-xl bg-white shadow-sm ring-1 ring-navy-200/5 lg:w-[750px]">
       <div className="px-4 py-6 sm:p-8">
         <h3 className="text-xl font-semibold leading-6 text-navy-900">{title}</h3>
         <div className="py-8">{children}</div>
