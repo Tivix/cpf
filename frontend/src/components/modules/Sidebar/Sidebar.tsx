3dacbda9c5a4b4da9cbe7de3a38bf73e23e0725d
Wiktoria Salamon <wiktoriajuliasalamon@gmail.com>
2024-07-01 12:47:56 +0200

    feat: add logo for collapsed sidebar

--- frontend/src/components/modules/Sidebar/Sidebar.tsx
+++ frontend/src/components/modules/Sidebar/Sidebar.tsx
@@ -17,39 +17,42 @@ export const Sidebar = () => {
 
   return (
     <nav
-      className={`z-50 h-screen ${open ? 'w-72' : 'w-16'} ${duration} flex flex-col border-r border-navy-200 bg-white px-3`}
+      className={`z-50 h-screen ${open ? 'w-72 px-3' : 'w-16 px-2'} ${duration} flex flex-col border-r border-navy-200 bg-white`}
     >
       <div className={`flex h-16 shrink-0 ${open ? 'justify-between' : 'justify-center'} mb-5 items-center`}>
         <div
-          className={`${duration} flex ${open ? 'opacity-1 ml-3 w-[104px] text-base' : 'w-0 text-[0px] opacity-0'} h-11 items-center justify-center rounded-full border border-navy-900 font-semibold`}
+          className={`${duration} flex ${open ? 'ml-3 w-[104px] text-base' : 'w-12 text-xs cursor-pointer'} h-11 items-center justify-center rounded-full border border-navy-900 font-semibold`}
+          onClick={open ? null : toggleSidebar}
         >
           CPF
         </div>
-        <Button onClick={toggleSidebar}>
-          <ChevronDoubleLeft className={`flex ${duration} ${open ? '' : 'rotate-180'}`} />
-        </Button>
+        {open && (
+            <Button onClick={toggleSidebar}>
+              <ChevronDoubleLeft className={`flex ${duration}`} />
+            </Button>
+        )}
       </div>
-      <ul role="list" className="flex flex-1 flex-col gap-y-7">
-        <li>
-          <ul role="list" className="space-y-2">
-            {navigation.map((item) => (
-              <li key={item.name}>
-                <Link
-                  href={item.href}
-                  className={generateClassNames(
-                    'flex items-center whitespace-nowrap rounded-md p-2 font-medium text-navy-600 hover:bg-blue-100',
-                    { 'bg-blue-100 text-blue-900': pathname.startsWith(item.href) },
-                  )}
-                >
-                  <div className="flex items-center gap-x-3">
-                    <item.icon className="h-5 w-5" />
-                    <span className={`${duration} leading-6 ${open ? 'text-sm' : 'text-[0px]'}`}>{item.name}</span>
-                  </div>
-                </Link>
-              </li>
-            ))}
-          </ul>
-        </li>
+      <ul role="list" className="flex flex-1 flex-col space-y-2">
+        {navigation.map((item) => (
+          <li key={item.name} className={`w-full flex ${open ? 'justify-start' : 'justify-center'}`}>
+            <Link
+              href={item.href}
+              className={generateClassNames(
+                'flex items-center whitespace-nowrap rounded-md p-2 font-medium text-navy-600 hover:bg-blue-100',
+                {
+                  'bg-blue-100 text-blue-900': pathname.startsWith(item.href),
+                  'w-full': open,
+                  'justify-center w-10': !open,
+                },
+              )}
+            >
+              <div className={`flex items-center ${open ? 'gap-x-3' : ''} `}>
+                <item.icon className="h-5 w-5" />
+                <span className={`${duration} leading-6 ${open ? 'text-sm' : 'text-[0px]'}`}>{item.name}</span>
+              </div>
+            </Link>
+          </li>
+        ))}
       </ul>
     </nav>
   );
