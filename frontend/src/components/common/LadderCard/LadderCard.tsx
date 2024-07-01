f47e212aa5d56c09d7e43034db759ae6c76e038f
Mikolaj Schatt <45075843+r1skz3ro@users.noreply.github.com>
2024-07-01 13:55:47 +0200

    fix: tile styling (#59)

--- frontend/src/components/common/LadderCard/LadderCard.tsx
+++ frontend/src/components/common/LadderCard/LadderCard.tsx
@@ -6,6 +6,6 @@ export const LadderCard = ({ ladderName, ladderSlug }: LadderCardInterface) => (
     href={`/library/${ladderSlug}`}
     className="flex h-44 cursor-pointer items-center justify-center rounded-2xl border border-navy-200 bg-white hover:bg-navy-100"
   >
-    <h2>{ladderName}</h2>
+    <h2 className="text-l text-navy-900">{ladderName}</h2>
   </Link>
 );
