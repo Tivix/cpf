8facbeff0c71f64c9e929af21b2da82bd31dcf94
Mikolaj Schatt <45075843+r1skz3ro@users.noreply.github.com>
2024-07-02 12:47:12 +0200

    [FE][CPF-39]: Scan frontend with Trivy (#47)
    * feat: api refactor, docker image push action
    
    * feat: scan and upload to security
    
    * chore: change owner name
    
    * feat: adjust envs

--- frontend/src/app/(app)/library/[ladder]/[bucket]/page.tsx
+++ frontend/src/app/(app)/library/[ladder]/[bucket]/page.tsx
@@ -1,34 +1,8 @@
 import { Breadcrumbs } from '@app/components/modules/Breadcrumbs';
-import { mapKeysToCamelCase } from '@app/utils';
 import { BucketDetails } from '@app/components/modules/BucketDetails';
-import { API_URLS } from '@app/api';
-import { Bucket, LadderBand } from '@app/types/common';
+import { getLadderName } from '@app/api/ladder';
+import { getBucketDetails } from '@app/api/bucket';
 import { routes } from '@app/constants';
-
-async function getBucketDetails(slug: string) {
-  const response = await fetch(`${API_URLS.library.buckets}/${slug}`);
-
-  if (!response.ok) {
-    throw new Error('Failed to fetch bucket details');
-  }
-  const data = await response.json();
-
-  return mapKeysToCamelCase<Bucket>(data);
-}
-
-async function getLadderName(slug: string) {
-  const response = await fetch(`${API_URLS.library.ladders}/${slug}`);
-
-  if (!response.ok) {
-    throw new Error('Failed to fetch ladder details');
-  }
-  const data = await response.json();
-
-  return mapKeysToCamelCase<{
-    ladderName: string;
-    bands: Record<string, LadderBand>;
-  }>(data).ladderName;
-}
 
 export default async function BucketDetailed({ params }: { params: { bucket: string; ladder: string } }) {
   const { bucket, ladder } = params;
@@ -48,3 +22,5 @@ export default async function BucketDetailed({ params }: { params: { bucket: str
     </div>
   );
 }
+
+export const dynamic = 'force-dynamic';
