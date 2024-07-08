a7e2afa7516100a27250c77bf3ee4a691224fca5
Mikolaj Schatt <45075843+r1skz3ro@users.noreply.github.com>
2024-07-08 13:07:01 +0200

    feat: set only one level to be expanded (#94)

--- frontend/src/components/modules/AdvancementLevel/AdvancementLevel.tsx
+++ frontend/src/components/modules/AdvancementLevel/AdvancementLevel.tsx
@@ -9,8 +9,8 @@ import { Markdown } from '@app/components/common/Markdown';
 import { Typography } from '@app/components/common/Typography';
 import { Button } from '@app/components/common/Button';
 
-export const AdvancementLevel: React.FC<AdvancementLevelProps> = ({ showVerticalLine, data }) => {
-  const { hideModal, openModal, toggleAccordionOpen, modalOpen, accordionOpen } = useAdvancementLevel();
+export const AdvancementLevel: React.FC<AdvancementLevelProps> = ({ showVerticalLine, data, open, onClick }) => {
+  const { hideModal, openModal, modalOpen } = useAdvancementLevel();
 
   const { advancementLevel, description, projects, categories } = data;
   const shouldBeExpandedByDefault = Object.keys(data.categories).length === 1;
@@ -20,16 +20,16 @@ export const AdvancementLevel: React.FC<AdvancementLevelProps> = ({ showVertical
       <div className="relative flex flex-col items-center">
         <button
           className="mb-2 mt-4 flex h-6 w-6 items-center justify-center rounded-full bg-blue-800 hover:opacity-50"
-          onClick={toggleAccordionOpen}
+          onClick={onClick}
         >
-          <ChevronRightIcon className={`h-3.5 w-3.5 text-white ${!accordionOpen ? 'rotate-90' : '-rotate-90'}`} />
+          <ChevronRightIcon className={`h-3.5 w-3.5 text-white ${!open ? 'rotate-90' : '-rotate-90'}`} />
         </button>
         {showVerticalLine && <div className="absolute left-3 top-12 h-[calc(100%-40px)] w-[1.5px] bg-blue-800" />}
       </div>
       <div className="mb-4 ml-4 flex w-full flex-col gap-4">
         <button
-          className={`flex w-full cursor-pointer flex-col gap-4 rounded-lg ${!accordionOpen && 'hover:bg-navy-50'}`}
-          onClick={toggleAccordionOpen}
+          className={`flex w-full cursor-pointer flex-col gap-4 rounded-lg ${!open && 'hover:bg-navy-50'}`}
+          onClick={onClick}
         >
           <Typography as="h3" variant="body-l/semibold">
             Advancement level {advancementLevel}
@@ -38,7 +38,7 @@ export const AdvancementLevel: React.FC<AdvancementLevelProps> = ({ showVertical
             {description}
           </Typography>
         </button>
-        {accordionOpen && (
+        {open && (
           <>
             {projects.length > 0 && (
               <Button variant="link" onClick={openModal} className="w-fit text-sm">
