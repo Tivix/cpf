f827ea264d07492d510f7fe8b11dc7ebfa99d63a
r1skz3ro <mikolaj.schatt@kellton.com>
2024-07-08 12:12:31 +0200

    feat: set only one level to be expanded

--- frontend/src/components/modules/AdvancementLevel/AdvancemetLevel.interface.ts
+++ frontend/src/components/modules/AdvancementLevel/AdvancemetLevel.interface.ts
@@ -3,12 +3,6 @@ import { AdvancementLevel } from '@app/types/library';
 export interface AdvancementLevelProps {
   data: AdvancementLevel;
   showVerticalLine: boolean;
-}
-
-export interface AdvancementLevelHooks {
-  hideModal: () => void;
-  openModal: () => void;
-  toggleAccordionOpen: () => void;
-  modalOpen: boolean;
-  accordionOpen: boolean;
+  open: boolean;
+  onClick: () => void;
 }
