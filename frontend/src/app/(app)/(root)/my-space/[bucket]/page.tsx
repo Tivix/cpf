ae39b22a82b3e6c78df8e452ce6ce4fdcbf080ac
Wiktoria Salamon <wiktoriajuliasalamon@gmail.com>
2024-07-29 11:03:33 +0200

    refactor: move mocked data outside the component

--- frontend/src/app/(app)/(root)/my-space/[bucket]/page.tsx
+++ frontend/src/app/(app)/(root)/my-space/[bucket]/page.tsx
@@ -3,6 +3,7 @@ import { MySpaceBucketDetails } from '@app/components/pages/mySpace/MySpaceBucke
 
 export default async function MySpaceBucketDetailed({ params }: { params: { bucket: string } }) {
   const { bucket } = params;
+  // TODO: get proper data from api
   const data = await getBucketDetails(bucket);
 
   return <MySpaceBucketDetails data={data} />;
