4c3de63290b75ba4731718adb85e9f231b0c0037
Wiktoria Salamon <39302106+wiktoriasalamon@users.noreply.github.com>
2024-07-29 10:51:01 +0200

    refactor: use Avatar component in PeopleTable (#132)

--- frontend/src/utils/index.ts
+++ frontend/src/utils/index.ts
@@ -2,4 +2,5 @@ export { generateClassNames } from './generateClassNames';
 export { mapKeysToCamelCase } from './mapKeysToCamelCase';
 export { readFile } from './file';
 export { getCroppedImg, createImage } from './canvas';
+export { getInitials } from './string';
 export { formatDate } from './date';
