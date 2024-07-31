ed128ec9d17c839e3f6a12d6e5beac185429b1ae
r1skz3ro <mikolaj.schatt@kellton.com>
2024-07-31 11:27:04 +0200

    feat: dropdown ui fixes

--- frontend/src/components/common/Input/Input.tsx
+++ frontend/src/components/common/Input/Input.tsx
@@ -5,45 +5,47 @@ import { generateClassNames } from '@app/utils';
 import { ErrorMessage } from '@hookform/error-message';
 import { AlertTriangleIcon } from '@app/static/icons/AlertTriangleIcon';
 
-export const Input: FC<InputProps> = memo(({ label, name, placeholder, leftIcon, options = {}, ...otherProps }) => {
-  const { formState, register, getFieldState } = useFormContext();
-  const error = getFieldState(name).error;
+export const Input: FC<InputProps> = memo(
+  ({ label, name, placeholder, leftIcon, className, options = {}, ...otherProps }) => {
+    const { formState, register, getFieldState } = useFormContext();
+    const error = getFieldState(name).error;
 
-  return (
-    <div className="flex w-full flex-col gap-y-3">
-      {label && (
-        <label className="text-navy-900" htmlFor={`input-${name}`}>
-          {label}
-        </label>
-      )}
-      <div className="relative rounded-md shadow-sm">
-        {leftIcon && (
-          <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">{leftIcon}</div>
+    return (
+      <div className={generateClassNames('flex w-full flex-col gap-y-3', className)}>
+        {label && (
+          <label className="text-navy-900" htmlFor={`input-${name}`}>
+            {label}
+          </label>
         )}
-        <input
-          className={generateClassNames(
-            'outline-black h-12 w-full rounded-xl border border-navy-200 px-4 outline-none focus:border-navy-700',
-            { 'border-red-600 focus:border-red-600': !!error },
-            { 'pl-8': leftIcon },
+        <div className="relative rounded-md shadow-sm">
+          {leftIcon && (
+            <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">{leftIcon}</div>
           )}
-          placeholder={placeholder}
-          id={`input-${name}`}
-          {...register(name, options)}
-          {...otherProps}
+          <input
+            className={generateClassNames(
+              'outline-black h-12 w-full rounded-xl border border-navy-200 px-4 outline-none focus:border-navy-700',
+              { 'border-red-600 focus:border-red-600': !!error },
+              { 'pl-8': leftIcon },
+            )}
+            placeholder={placeholder}
+            id={`input-${name}`}
+            {...register(name, options)}
+            {...otherProps}
+          />
+        </div>
+        <ErrorMessage
+          errors={formState.errors}
+          name={name}
+          render={({ message }) => (
+            <div className="flex items-center gap-x-2">
+              <AlertTriangleIcon />
+              <div className="text-sm text-red-600 first-letter:uppercase">{message}</div>
+            </div>
+          )}
         />
       </div>
-      <ErrorMessage
-        errors={formState.errors}
-        name={name}
-        render={({ message }) => (
-          <div className="flex items-center gap-x-2">
-            <AlertTriangleIcon />
-            <div className="text-sm text-red-600 first-letter:uppercase">{message}</div>
-          </div>
-        )}
-      />
-    </div>
-  );
-});
+    );
+  },
+);
 
 Input.displayName = 'Input';
