5a1ea82331bbee94ac896cc1d43dda951756a6e6
Mikolaj Schatt <45075843+r1skz3ro@users.noreply.github.com>
2024-08-01 14:06:45 +0200

    [FE][CPF-126] Input fixes (#135)
    * feat: listbox and input fixes
    
    * feat: add ClassName type

--- frontend/src/types/common.ts
+++ frontend/src/types/common.ts
@@ -1,3 +1,5 @@
+import { HTMLProps } from 'react';
+
 export interface Point {
   x: number;
   y: number;
@@ -7,3 +9,5 @@ export interface Option {
   id: string;
   name: string;
 }
+
+export type ClassName = HTMLProps<HTMLElement>['className'];
