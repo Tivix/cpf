f35872c73afa46ed9bc1eeaa15f6920c020cd444
r1skz3ro <mikolaj.schatt@kellton.com>
2024-08-29 10:28:38 +0200

    chore: remove unused api

--- frontend/src/api/index.ts
+++
@@ -1,9 +0,0 @@
-const baseUrl = process.env.NEXT_PUBLIC_API_BASE_URL;
-
-export const API_URLS = {
-  library: {
-    ladders: `${baseUrl}/library/ladders`,
-    buckets: `${baseUrl}/library/buckets`,
-  },
-  people: `${baseUrl}/people`,
-};
